dda0185b6faac1872fae2fae3b9a1ab1883099c7
ItaloSixx <anonimosbang@gmail.com>
2024-10-31 20:37:33 -0300

    feat: cadastrar e mostrar cupons

---
+++ app/Http/Controllers/CouponsController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CouponRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CouponsController extends Controller
+{
+
+    public function index()
+    {
+        $coupons = DB::table('coupons')->whereNull('deleted_at')->get();
+
+        return response()->json($coupons, 200);
+    }
+
+
+    public function store(CouponRequest $request)
+    {
+    $data = $request->validated();
+
+    $coupon = DB::table('coupons')->insert([
+        'code' => $data['code'],
+        'discount_value' => $data['discount_value'],
+        'created_at' => now(),
+        'updated_at' => now()
+    ]);
+
+    if (!$coupon) {
+        return response()->json([
+            'message' => 'Erro ao criar o cupom'
+        ], 500);
+    }
+
+    return response()->json([
+        'message' => 'Cupom criado com sucesso'
+    ], 201);
+    }
+
+
+    
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
