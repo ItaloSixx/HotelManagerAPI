124106583631ad5babff1fd69427b0e3ebc98174
ItaloSixx <anonimosbang@gmail.com>
2024-10-31 17:17:01 -0300

    buscar user by id, update e softDelete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 
     public function index()
     {
-        $users = DB::table('users')->get();
+        $users = DB::table('users')->whereNull('deleted_at')->get();
 
         return response()->json($users, 200);
     }
@@ -41,17 +41,59 @@ class UserController extends Controller
 
     public function show(string $id)
     {
+        $user = DB::table('users')
+                ->where('id', $id)
+                ->whereNull('deleted_at')
+                ->first();
+
+        if(!$user){
+            return response()->json([
+                'message' => 'usuário não encontrado'
+            ], 404);
+        }
+
+        return response()->json($user,200);
     }
 
 
-    public function update(Request $request, string $id)
-    {
-        //
+    public function update(UserStoreRequest $request, string $id)
+{
+    $data = $request->validated();
+
+    $userPut = DB::table('users')->where('id', $id)
+                ->update([
+                    'name' => $data['name'],
+                    'email' => $data['email'],
+                    'password' => bcrypt($data['password']),
+                    'role' => $data['role'],
+                    'updated_at' => now()
+                ]);
+
+    if ($userPut === 0) {
+        return response()->json([
+            'message' => 'Usuário não encontrado ou não atualizado'
+        ], 404);
     }
+
+    return response()->json([
+        'message' => 'Atualizado com sucesso'
+    ], 200);
+}
+
 
 
     public function destroy(string $id)
     {
-        //
+        $userExist = DB::table('users')->where('id', $id)->update(['deleted_at'=> now()]);
+
+        if(!$userExist){
+            return response()->json([
+                'message' => 'Usuário não encontrado/excluido'
+            ], 404);
+        }
+
+        return response()->json([
+            'Message' => 'Usuário excluído com sucesso'
+        ], 200);
     }
 }
--- app/Http/Requests/UserStoreRequest.php
+++ app/Http/Requests/UserStoreRequest.php
@@ -6,26 +6,21 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UserStoreRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
-    {
-        return [
-            'name' =>'required',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|string|min:8',
-            'role' => 'required'
-        ];
-    }
+{
+    $userId = $this->route('user');
+    return [
+        'name' => 'required',
+        'email' => "required|email|unique:users,email,{$userId},id",
+        'password' => 'required|string|min:8',
+        'role' => 'required'
+    ];
+}
+
 }
