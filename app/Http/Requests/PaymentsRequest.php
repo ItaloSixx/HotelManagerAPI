b6bafa42974633d743a7bdc758e33bb4875fa534
ItaloSixx <anonimosbang@gmail.com>
2024-11-02 15:55:15 -0300

    style: form request mensagens payments

--- app/Http/Requests/PaymentsRequest.php
+++ app/Http/Requests/PaymentsRequest.php
@@ -22,4 +22,20 @@ class PaymentsRequest extends FormRequest
             'paid' => 'required|boolean'
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'reserveId.required' => 'O campo ID da reserva é obrigatório.',
+            'reserveId.exists' => 'A reserva fornecida não existe.',
+            'value.required' => 'O campo valor é obrigatório.',
+            'value.numeric' => 'O valor deve ser numérico.',
+            'value.min' => 'O valor deve ser no mínimo 0.',
+            'method.required' => 'O campo método é obrigatório.',
+            'method.string' => 'O método deve ser uma string.',
+            'paid.required' => 'O campo pago é obrigatório.',
+            'paid.boolean' => 'O campo pago deve ser verdadeiro ou falso.',
+        ];
+    }
+
 }
