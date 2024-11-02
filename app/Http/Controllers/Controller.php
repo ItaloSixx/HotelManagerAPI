a361d661bc056da4ee2fa5223bffd26a960b62c5
ItaloSixx <anonimosbang@gmail.com>
2024-11-02 16:44:34 -0300

    chore: instalando swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Server(url="http://localhost/api/"),
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="L5 OpenApi",
+ *      description="L5 Swagger OpenApi description",
+ *      @OA\Contact(
+ *          email="italojefer55@gmail.com"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * ),
+ *
+ */
 
 abstract class Controller
 {
