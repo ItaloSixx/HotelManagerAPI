388d0b02bb1b1e5ac6472b05b138d4c93157feb9
ItaloSixx <anonimosbang@gmail.com>
2024-11-02 16:00:41 -0300

    style: form request mensagens users

--- app/Http/Requests/UserStoreRequest.php
+++ app/Http/Requests/UserStoreRequest.php
@@ -13,14 +13,29 @@ class UserStoreRequest extends FormRequest
     }
 
     public function rules(): array
+    {
+        $userId = $this->route('user');
+        return [
+            'name' => 'required',
+            'email' => "required|email|unique:users,email,{$userId},id",
+            'password' => 'required|string|min:8',
+            'role' => 'required'
+        ];
+    }
+
+    public function messages(): array
 {
-    $userId = $this->route('user');
     return [
-        'name' => 'required',
-        'email' => "required|email|unique:users,email,{$userId},id",
-        'password' => 'required|string|min:8',
-        'role' => 'required'
+        'name.required' => 'O campo nome é obrigatório.',
+        'email.required' => 'O campo email é obrigatório.',
+        'email.email' => 'O email fornecido deve ser um endereço de email válido.',
+        'email.unique' => 'Este email já está em uso.',
+        'password.required' => 'O campo senha é obrigatório.',
+        'password.string' => 'A senha deve ser uma string.',
+        'password.min' => 'A senha deve ter pelo menos 8 caracteres.',
+        'role.required' => 'O campo papel (role) é obrigatório.'
     ];
 }
+
 
 }
