d48bf8e171c9e494dfaad4869800092501363d79
ItaloSixx <anonimosbang@gmail.com>
2024-11-04 17:05:03 -0300

    docs: arquivos necessários

--- API_HOTEL_LARAVEL/routes/api.php
+++ API_HOTEL_LARAVEL/routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\PaymentsController;
 use App\Http\Controllers\ReserveGuestsController;
 use App\Http\Controllers\ReservesController;
 use App\Http\Controllers\RoomController;
-use App\Http\Controllers\UserController;    
+use App\Http\Controllers\UserController;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,30 +24,30 @@ Route::get('/status', function(){
     200);
 })->middleware('auth:sanctum');
 
-Route::middleware(['auth:sanctum', RoleMiddleware::class.':admin|receptionist'])->group(function(){
-    Route::apiResource('coupons', CouponsController::class);
-    Route::apiResource('reserves', ReservesController::class);
-    Route::apiResource('dailies', DailiesController::class);
-    Route::apiResource('guests', GuestsController::class);
-    Route::apiResource('payments', PaymentsController::class);
-    Route::apiResource('rooms', RoomController::class);
-    Route::post('/reserves/{reserveId}/addGuest', [ReserveGuestsController::class, 'addGuest'])->name('addGuest');
-    Route::get('/reserves/{reserveId}/getGuest', [ReserveGuestsController::class, 'getGuests'])->name('getGuests');
-    Route::get('/reserves/{guestId}/getReserve', [ReserveGuestsController::class, 'getReserves'])->name('getReserve');
-    Route::delete('/reserves/{reserveId}/{guestId}/rmvGuest', [ReserveGuestsController::class, 'rmvGuest'])->name('rmvGuest');
-});
-
-Route::middleware(['auth:sanctum', RoleMiddleware::class.':admin'])->group(function(){
-    Route::apiResource('hotels', HotelsController::class);
-    Route::get('users', [UserController::class, 'index']);
-    Route::get('users/{user}', [UserController::class, 'show']);
-    Route::put('users/update/{user}', [UserController::class, 'update']);
-    Route::patch('users/update{user}', [UserController::class, 'update']);
-    Route::delete('users/{user}', [UserController::class, 'destroy']);
+    Route::middleware(['auth:sanctum', RoleMiddleware::class.':admin|receptionist'])->group(function(){
+        Route::apiResource('coupons', CouponsController::class);
+        Route::apiResource('reserves', ReservesController::class);
+        Route::apiResource('dailies', DailiesController::class);
+        Route::apiResource('guests', GuestsController::class);
+        Route::apiResource('payments', PaymentsController::class);
+        Route::apiResource('rooms', RoomController::class);
+        Route::post('/reserves/{reserveId}/addGuest', [ReserveGuestsController::class, 'addGuest'])->name('addGuest');
+        Route::get('/reserves/{reserveId}/getGuest', [ReserveGuestsController::class, 'getGuests'])->name('getGuests');
+        Route::get('/reserves/{guestId}/getReserve', [ReserveGuestsController::class, 'getReserves'])->name('getReserve');
+        Route::delete('/reserves/{reserveId}/{guestId}/rmvGuest', [ReserveGuestsController::class, 'rmvGuest'])->name('rmvGuest');
     });
 
-Route::post('/users', [UserController::class, 'store']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::middleware(['auth:sanctum', RoleMiddleware::class.':admin'])->group(function(){
+        Route::apiResource('hotels', HotelsController::class);
+        Route::get('users', [UserController::class, 'index']);
+        Route::get('users/{user}', [UserController::class, 'show']);
+        Route::put('users/update/{user}', [UserController::class, 'update']);
+        Route::patch('users/update{user}', [UserController::class, 'update']);
+        Route::delete('users/{user}', [UserController::class, 'destroy']);
+        });
+
+    Route::post('/users', [UserController::class, 'store']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 
